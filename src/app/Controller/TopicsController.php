01dd59b71128086b708cdaba5bf7a85a970eb846
yuuhei-koutoku <yhkt.0627@gmail.com>
2023-09-24 00:12:27 +0900

    execute bake

---
+++ src/app/Controller/TopicsController.php
@@ -0,0 +1,107 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Topics Controller
+ *
+ * @property Topic $Topic
+ * @property PaginatorComponent $Paginator
+ */
+class TopicsController extends AppController {
+
+/**
+ * Components
+ *
+ * @var array
+ */
+	public $components = array('Paginator');
+
+/**
+ * index method
+ *
+ * @return void
+ */
+	public function index() {
+		$this->Topic->recursive = 0;
+		$this->set('topics', $this->Paginator->paginate());
+	}
+
+/**
+ * view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function view($id = null) {
+		if (!$this->Topic->exists($id)) {
+			throw new NotFoundException(__('Invalid topic'));
+		}
+		$options = array('conditions' => array('Topic.' . $this->Topic->primaryKey => $id));
+		$this->set('topic', $this->Topic->find('first', $options));
+	}
+
+/**
+ * add method
+ *
+ * @return void
+ */
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Topic->create();
+			if ($this->Topic->save($this->request->data)) {
+				$this->Flash->success(__('The topic has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Flash->error(__('The topic could not be saved. Please, try again.'));
+			}
+		}
+		$categories = $this->Topic->Category->find('list');
+		$this->set(compact('categories'));
+	}
+
+/**
+ * edit method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function edit($id = null) {
+		if (!$this->Topic->exists($id)) {
+			throw new NotFoundException(__('Invalid topic'));
+		}
+		if ($this->request->is(array('post', 'put'))) {
+			if ($this->Topic->save($this->request->data)) {
+				$this->Flash->success(__('The topic has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Flash->error(__('The topic could not be saved. Please, try again.'));
+			}
+		} else {
+			$options = array('conditions' => array('Topic.' . $this->Topic->primaryKey => $id));
+			$this->request->data = $this->Topic->find('first', $options);
+		}
+		$categories = $this->Topic->Category->find('list');
+		$this->set(compact('categories'));
+	}
+
+/**
+ * delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function delete($id = null) {
+		if (!$this->Topic->exists($id)) {
+			throw new NotFoundException(__('Invalid topic'));
+		}
+		$this->request->allowMethod('post', 'delete');
+		if ($this->Topic->delete($id)) {
+			$this->Flash->success(__('The topic has been deleted.'));
+		} else {
+			$this->Flash->error(__('The topic could not be deleted. Please, try again.'));
+		}
+		return $this->redirect(array('action' => 'index'));
+	}
+}
